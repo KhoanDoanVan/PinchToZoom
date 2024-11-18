1c7c63be728951c7901f344ca790e63ef4d13e49
khoanisdev <doanvankhoan124@gmail.com>
2024-11-18 19:07:46 +0700

    Initial Commit

---
+++ PinchToZoom/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PinchToZoom
+//
+//  Created by Đoàn Văn Khoan on 18/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PinchToZoom/PinchToZoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchToZoomApp.swift
+//  PinchToZoom
+//
+//  Created by Đoàn Văn Khoan on 18/11/24.
+//
+
+import SwiftUI
+
+@main
+struct PinchToZoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
